11a6aa113fbc1bbcaca5be094b8777e77e82197f
Kagaya <kagaya85@foxmail.com>
2018-12-31 17:42:57 +0800

    send-level4

--- 多进程对内存空间的不同使用方法/01/send-level4.cpp
+++ 多进程对内存空间的不同使用方法/01/send-level4.cpp
@@ -2,46 +2,62 @@
 
 using namespace std;
 
+const char* namestr = "send-level-4";
+
 int main()
 {
     int rfd, wfd;
-    char* rfilename = "5to4.txt";
-    char* wfilename = "4to3.txt";
+    char* rfilename = "5to4.dat";
+    char* wfilename = "4to3.dat";
+    int len, bufsize = 2048;
+    char* buf[bufsize];
+    TCPHead tcpHead;
 
     signal(SIGREADY, signal_handle);
 
     pause();
+
+    tcpHead = getTcpHead();    
+    unsigned short offset = tcpOffset(tcpHead);
 
     rfd = open(rfilename, O_RDONLY);
     if(rfd < 0){
         cerr << "open " << filename << " error" << endl;
         exit(EXIT_FAILURE);
     }
+    flock(rfd, LOCK_SH);
+    len = read(rfd, &buf[TCP_HEAD_MIN_LEN + offset], bufsize);
+    if(len <= 0) {
+        cerr << namestr << " read error" << endl;
+        exit(EXIT_FAILURE);
+    }
+    close(rfd);
+    remove(rfilename);
+
+    cout << "send-level-4 tcp head:" << endl;
+    analyzeTcphead(tcpHead, &buf[TCP_HEAD_MIN_LEN]);
 
     wfd = open(wfilename, O_WRONLY);
     if(wfd < 0){
         cerr << "open " << filename << " error" << endl;
         exit(EXIT_FAILURE);
     }
+    flock(wfd, LOCK_EX);
+    write(wfd, buf, TCP_HEAD_MIN_LEN + offset + len);
+    close(wfd);
 
-    int ret, bufsize = 1000;
-    char* buf[bufsize];
-
-    ret = read(rfd, buf, bufsize);
-    if (ret > 0) {
-        cout << "send-level4 has read " << ret << " byte(s) from level5" << endl;
-        /* do someting */
-         
-    }
-    else {
-        cout << "send-level4 read error" << endl;
+    int pid;
+    while ((pid = getPidByName("send-level-3")) == 0) {
+        sleep(1);
     }
 
+    kill(pid, SIGREADY);
 
-    cout << "send-level4 complete" << endl;
+    cout << namestr << " complete" << endl;
+    return 0;
 }
 
 void signal_handle(int signal)
 {
-    cout << "send-level4 " << "get ready signal" << endl;
+    cout << namestr << " get ready signal" << endl;
 }
