13ff1d1bb35b91bc7fec6f63b1b52c97d496b539
Kagaya <kagaya85@foxmail.com>
2018-12-31 16:43:49 +0800

    recv-level-1

--- 多进程对内存空间的不同使用方法/01/recv-level-1.cpp
+++ 多进程对内存空间的不同使用方法/01/recv-level-1.cpp
@@ -2,30 +2,32 @@
 
 using namespace std;
 
-int main()
-{
+int main() {
     int rfd, wfd;
     char* rfilename = "network.dat";
+    char* wfilename = "1to2.dat";
+    int ret, bufsize = 2048;
+    char* buf[bufsize];
 
     signal(SIGREADY, signal_handle);
 
     pause();
 
     rfd = open(rfilename, O_RDONLY);
-    if(rfd < 0){
+    if (rfd < 0) {
         cerr << "open " << filename << " error" << endl;
         exit(EXIT_FAILURE);
     }
 
     wfd = open(wfilename, O_WRONLY);
-    if(wfd < 0){
+    if (wfd < 0) {
         cerr << "open " << filename << " error" << endl;
         exit(EXIT_FAILURE);
     }
 
-    int ret, bufsize = 1000;
-    char* buf[bufsize];
-    while(true) {
+    flock(rfd, LOCK_SH);
+    flock(wfd, LOCK_EX);
+    while (true) {
         ret = read(rfd, buf, bufsize);
         if (ret > 0)
             write(wfd, buf, ret);
@@ -33,10 +35,18 @@ int main()
             break;
     }
 
+    int pid;
+    while ((pid = getPidByName("recv-level-2")) == 0) {
+        sleep(1);
+    }
+
+    kill(pid, SIGREADY);
+
     cout << "recv-level1 complete" << endl;
+    return 0;
 }
 
-void signal_handle(int signal)
-{
-    cout << "recv-level1 " << "get ready signal" << endl;
+void signal_handle(int signal) {
+    cout << "recv-level1 "
+         << "get ready signal" << endl;
 }
